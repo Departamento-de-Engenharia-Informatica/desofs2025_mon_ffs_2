8c0be9c61c6070c59a6448e6e1d34bb6541966cb
Paulo Abreu <abreupaulo130499@gmail.com>
2025-06-10 12:10:15 +0100

    Fix Delivery

--- project/AMAPP.API/Services/Interfaces/IDeliveryService.cs
+++ project/AMAPP.API/Services/Interfaces/IDeliveryService.cs
@@ -1,9 +1,24 @@
 ﻿using AMAPP.API.DTOs.Delivery;
 
-public interface IDeliveryService
+namespace AMAPP.API.Services.Interfaces
 {
-    Task<List<DeliveryDto>> GetAllAsync();
-    Task<DeliveryDto?> GetByIdAsync(int id);
-    Task<DeliveryDto> CreateAsync(CreateDeliveryDto dto);
-    Task<DeliveryDto?> UpdateAsync(DeliveryDto dto);
+    public interface IDeliveryService
+    {
+        // Métodos existentes
+        Task<IEnumerable<DeliveryDto>> GetAllAsync();
+        Task<DeliveryDto?> GetByIdAsync(int id, string userId = null);
+        Task<DeliveryDto> CreateAsync(CreateDeliveryDto dto);
+        Task<DeliveryDto?> UpdateAsync(int id, UpdateDeliveryDto dto);
+        Task<bool> DeleteAsync(int id);
+
+        // Novos métodos seguindo o padrão do OrderService
+        Task<IEnumerable<DeliveryDto>> GetDeliveriesByProducerAsync(int producerId);
+        Task<IEnumerable<DeliveryDto>> GetDeliveriesByCoProducerAsync(int coproducerId);
+        Task<IEnumerable<DeliveryDto>> GetDeliveriesByUserIdAsync(string userId);
+
+        // Métodos de autorização
+        Task<bool> IsUserAuthorizedForCoproducerAsync(string userId, int coproducerId);
+        Task<bool> IsUserAuthorizedForProducerAsync(string userId, int producerId);
+        Task<bool> CanUserAccessDeliveryAsync(int deliveryId, string userId);
+    }
 }
